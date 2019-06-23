51598667822a3d84af4a0fe853236eb93846463c
Brittany Javalera <brittjavalera@gmail.com>
2019-06-23 17:28:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,24 +11,13 @@ class Dog
   def name=(dog_name)
     @dog_name = dog_name
   end
- 
-end
-
-class Person
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  
+  def breed
+    @dog_breed
   end
  
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
   end
  
 end
