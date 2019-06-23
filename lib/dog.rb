b46f1b5f003d9042932ae5d63453f1ebaa880593
Brittany Javalera <brittjavalera@gmail.com>
2019-06-23 17:30:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,16 @@ class Dog
     @name
   end
  
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
   
   def breed
-    @dog_breed
+    @breed
   end
  
-  def breed=(dog_breed)
-    @dog_breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
  
 end
